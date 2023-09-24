5122abd0bd396a2747e088681106e27331181203
Oleksandr Aleksieiev <frontdev1991@gmail.com>
2023-09-24 14:12:08 +0200

    added content page

--- src/views/Content/index.tsx
+++ src/views/Content/index.tsx
@@ -1,14 +1,67 @@
-import React, {FC, ReactElement} from 'react';
+import React, {FC, ReactElement, useState} from 'react';
 import AppLayout from '../../layouts/App.layout';
 import MainLayout from '../../layouts/Main.layout';
 import ContainerLayout from '../../layouts/Container.layout';
+import {Button, Icon, IconButton, Stack, Text} from '@chakra-ui/react';
+import {RefreshCw} from 'lucide-react';
 
 const Content: FC = (): ReactElement => {
+  const [hero, setHero] = useState<string>('');
+  const [word, setWord] = useState<string>('');
+
+  const nextHero = () => {
+    setHero('');
+  };
+
+  const nextWord = () => {
+    setWord('');
+  };
+
   return (
     <AppLayout>
       <MainLayout>
         <ContainerLayout>
-          <div>Content</div>
+          <Stack direction={'column'} alignItems={'center'} justifyContent={'center'} w={'full'} h={'full'} spacing={12}>
+            <Stack direction={'row'} alignItems={'center'} justifyContent={'space-between'} spacing={6} overflow={'hidden'}>
+              <Text
+                fontSize={32}
+                as={'strong'}
+                whiteSpace={'nowrap'}
+                textOverflow={'ellipsis'}
+                overflow={'hidden'}>
+                {hero}
+              </Text>
+
+              <IconButton
+                onClick={nextHero}
+                colorScheme={'whatsapp'}
+                variant={'outline'}
+                boxShadow={'md'}
+                title={'Next hero'}
+                aria-label={'Next hero'}
+                icon={<Icon as={RefreshCw}/>}/>
+            </Stack>
+
+            <Stack direction={'column'} alignItems={'center'} justifyContent={'center'} spacing={6} overflow={'hidden'}>
+              <Text
+                fontSize={24}
+                whiteSpace={'nowrap'}
+                textOverflow={'ellipsis'}
+                overflow={'hidden'}>
+                {word}
+              </Text>
+
+              <Button
+                onClick={nextWord}
+                colorScheme={'telegram'}
+                variant={'outline'}
+                boxShadow={'md'}
+                title={'Next word'}
+                aria-label={'Next word'}>
+                Next word
+              </Button>
+            </Stack>
+          </Stack>
         </ContainerLayout>
       </MainLayout>
     </AppLayout>

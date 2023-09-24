402e33ab12d65e6632126d262e8ea9d44c9b7fc1
Oleksandr Aleksieiev <frontdev1991@gmail.com>
2023-09-24 15:45:36 +0200

    added words logic

--- src/views/Content/index.tsx
+++ src/views/Content/index.tsx
@@ -1,11 +1,12 @@
-import React, {FC, ReactElement, useState} from 'react';
+import React, {FC, ReactElement, useEffect, useState} from 'react';
 import AppLayout from '../../layouts/App.layout';
 import MainLayout from '../../layouts/Main.layout';
 import ContainerLayout from '../../layouts/Container.layout';
-import {Button, Icon, IconButton, Stack, Text} from '@chakra-ui/react';
-import {RefreshCw} from 'lucide-react';
+import {Button, Icon, Stack, Text} from '@chakra-ui/react';
+import {ArrowBigRight, RefreshCw} from 'lucide-react';
 import {TypeHeroes} from '../../types/heroes.type';
 import {TypeWords} from '../../types/words.type';
+import {createRandomNumber} from '../../utils/createRandomNumber';
 
 const Content: FC = (): ReactElement => {
   const [heroes, setHeroes] = useState<TypeHeroes>([]);
@@ -13,20 +14,42 @@ const Content: FC = (): ReactElement => {
   const [hero, setHero] = useState<string>('');
   const [word, setWord] = useState<string>('');
 
-  const nextHero = () => {
-    setHero('');
+  const init = async (): Promise<void> => {
+    const heroesEN = await import('../../db/heroes.en.json');
+    const wordsEN = await import('../../db/words.en.json');
+
+    setHeroes(heroesEN.heroes);
+    setWords(wordsEN.words);
   };
 
-  const nextWord = () => {
-    setWord('');
+  const nextHero = (): void => {
+    const {value} = createRandomNumber(0, heroes.length - 1);
+    setHero(heroes[value]);
   };
+
+  const nextWord = (): void => {
+    const {value} = createRandomNumber(0, words.length - 1);
+    setWord(words[value]);
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  useEffect(() => {
+    nextHero();
+  }, [heroes]);
+
+  useEffect(() => {
+    nextWord();
+  }, [words]);
 
   return (
     <AppLayout>
       <MainLayout>
         <ContainerLayout>
           <Stack direction={'column'} alignItems={'center'} justifyContent={'center'} w={'full'} h={'full'} spacing={12}>
-            <Stack direction={'row'} alignItems={'center'} justifyContent={'space-between'} spacing={6} overflow={'hidden'}>
+            <Stack direction={'column'} alignItems={'center'} justifyContent={'center'} spacing={6} overflow={'hidden'}>
               <Text
                 fontSize={32}
                 as={'strong'}
@@ -36,14 +59,16 @@ const Content: FC = (): ReactElement => {
                 {hero}
               </Text>
 
-              <IconButton
-                onClick={nextHero}
+              <Button
+                onClick={() => nextHero()}
                 colorScheme={'whatsapp'}
                 variant={'outline'}
                 boxShadow={'md'}
+                rightIcon={<Icon as={RefreshCw}/>}
                 title={'Next hero'}
-                aria-label={'Next hero'}
-                icon={<Icon as={RefreshCw}/>}/>
+                aria-label={'Next hero'}>
+                Next hero
+              </Button>
             </Stack>
 
             <Stack direction={'column'} alignItems={'center'} justifyContent={'center'} spacing={6} overflow={'hidden'}>
@@ -56,10 +81,11 @@ const Content: FC = (): ReactElement => {
               </Text>
 
               <Button
-                onClick={nextWord}
+                onClick={() => nextWord()}
                 colorScheme={'telegram'}
                 variant={'outline'}
                 boxShadow={'md'}
+                rightIcon={<Icon as={ArrowBigRight}/>}
                 title={'Next word'}
                 aria-label={'Next word'}>
                 Next word
